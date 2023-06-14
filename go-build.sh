5af3970c1cfa6a20319789c1f1ecdc39c97e6fdf
DQNEO <dqneoo@gmail.com>
2023-06-14 10:18:03 +0900

    Add another file and put together multi grep calls

--- go-build.sh
+++ go-build.sh
@@ -126,10 +126,7 @@ function list_maching_files_in_dir() {
   log "  allfiles=(${ary[@]})"
   echo "$allfiles" |\
     grep -v -E '_test\.go' |
-    grep -v -E 'go_below_11.\.go' |
-    grep -v -E 'error_1_13\.go' |
-    grep -v -E 'bypasssafe.go' |
-    grep -v -E 'ct_win.go' | # // https://github.com/daviddengcn/go-colortext/blob/master/ct_win.go#L1
+    grep -v -E '(bypasssafe|ct_win|bytestostr_nounsafe|error_1_13|go_below_11.)\.go' |
     grep -v -E "_(${NON_GOOS_LIST})(\.|_)" |
     grep -v -E "_(${NON_GOARCH_LIST})\.(go|s)"
 }
